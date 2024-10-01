e624ade6f83f0df0e1d7787de5f9d438f410c4a2
Deepikasai Mettu <161874594+deepikasai-mettu@users.noreply.github.com>
2024-10-01 16:13:09 +0000

    Updating stackname

--- lib/constants.ts
+++ lib/constants.ts
@@ -7,5 +7,5 @@ export const cognitoDomainName = "ffio-auth"
 // when setting up a sign-in provider in Cognito
 // make sure to leave it blank if you do not actually have an SSO provider configured in Cognito!
 export const OIDCIntegrationName = ""
-// this MUST be unique to your account also case sensitive!
+// this MUST be unique to your account also case sensitive!!!
 export const stackName = "ffioStack"
