5f0e8579b0961e7fcf90400466371e6c5e072bd3
Serena Green <serenagreen@MacBook-Pro-4.local>
2024-10-01 14:20:16 -0400

    pls

--- lib/user-interface/app/src/common/constants.ts
+++ lib/user-interface/app/src/common/constants.ts
@@ -41,4 +41,4 @@ export const feedbackTypes = [
   {label: "Other", value:"other", disabled: false}
 ]
 
-export const CHATBOT_NAME = "Is it working";
+export const CHATBOT_NAME = "Hope this is working again";
